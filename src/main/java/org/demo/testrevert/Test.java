aa675f21dc9948b20c0eea5cdf25df9138dc4306
Aleksandr Kamenev <144233016+AleksandrKamen@users.noreply.github.com>
2024-08-13 08:43:15 +0300

    Revert "Add files"

--- src/main/java/org/demo/testrevert/Test.java
+++
@@ -1,9 +0,0 @@
-package org.demo.testrevert;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println();
-    }
-}
