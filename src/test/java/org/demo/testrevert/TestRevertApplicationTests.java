e37d9d64cd3ad1973db1e295b3c216099dcd3988
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-08-13 08:40:06 +0300

    first commit

---
+++ src/test/java/org/demo/testrevert/TestRevertApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.testrevert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRevertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
